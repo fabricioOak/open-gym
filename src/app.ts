7d20295ddb98ecd6c39403ccfb8dacfccc508a21
Fabricio Carvalho <fcarvalho1999@gmail.com>
2025-09-05 15:42:14 -0300

    error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import fastify from "fastify";
+import { routes } from "./routes";
+import { z, ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify({
   logger: {
@@ -9,4 +12,21 @@ export const app = fastify({
       },
     },
   },
+});
+
+routes(app);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    const issues = z.formatError(error);
+    reply.status(400).send({ message: "Validation error", issues });
+  }
+
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
+  } else {
+    // TODO: Should log to a tool like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: "Internal server error" });
 });
